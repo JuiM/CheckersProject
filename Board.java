d7ab78f4fdfc8d5866e77bddf410ba468a172432
JuiM <juiangel@yahoo.com>
2014-04-22 12:50:39 -0700

    Update Board.java

--- Board.java
+++ Board.java
@@ -2,5 +2,14 @@ public class Board {
 	int[][] board;
 	public Board() {
 		board = new int[10][10];
+		setBackground(Color.BLACK);
+         addMouseListener(this);
+         resignButton = new JButton("Resign");
+         resignButton.addActionListener(this);
+         newGameButton = new JButton("New Game");
+         newGameButton.addActionListener(this);
+         message = new JLabel("",JLabel.CENTER);
+         message.setFont(new  Font("Serif", Font.BOLD, 14));
+         message.setForeground(Color.green);
 	}
 }
