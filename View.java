829099301cc76873cea5b361bc36be1eb7cf25d9
JuiM <juiangel@yahoo.com>
2014-04-25 19:29:35 -0700

    Update View.java

--- View.java
+++ View.java
@@ -1,10 +1,10 @@
-import java.awt.Color;
-import java.awt.Graphics;
+import java.awt.*;
+import java.awt.event.*;
 
 import javax.swing.JFrame;
 
 
-public class View extends JFrame{
+public class View extends JFrame implements MouseListener{
 
 	public View() {
 		super("");
@@ -15,4 +15,30 @@ public class View extends JFrame{
 		g.setColor(Color.WHITE);
 		g.fillRect(0, 0, 500, 530);
 	}
+	 /**
+       * Respond to a user click on the board.  If no game is in progress, show 
+       * an error message.  Otherwise, find the row and column that the user 
+       * clicked and call clickSquare() to handle it.
+       */
+      public void mousePressed(MouseEvent evt) {
+      	// should I create a gameInProgress method??
+         if (gameInProgress == false)
+            message.setText("Click \"New Game\" to start a new game.");
+        // what else do we need when the mouse is pressed
+        // else get the row and col and then click it
+         else {
+            int col = (evt.getX() - 2) / 20;
+            int row = (evt.getY() - 2) / 20;
+            if (col >= 0 && col < 8 && row >= 0 && row < 8)
+               //clickSquare(row,col); should I have a 
+               //clickSquare() method?
+         }
+      }
+      public void mouseReleased(MouseEvent evt) { }
+      // Do we still need the game.clicked(x,y) thing?
+      public void mouseClicked(MouseEvent evt) { }
+      public void mouseEntered(MouseEvent evt) { }
+      public void mouseExited(MouseEvent evt) { }
+      // should I have a setUpGame() method?
+      //where we can set up the black and red 
 }
